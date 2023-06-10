b5b24b668da161fae6b8016a5d7bc791a82eefdd
thuyiya <thusitha.4t@gmail.com>
2023-06-10 12:46:30 +0530

    express added

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000
+
+app.get("/", function (req, res) {
+    res.json({
+        message: "Working"
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
